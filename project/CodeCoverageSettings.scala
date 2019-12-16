5dd3050e209fb7d74ada5f2dfad846a9090f4aa8
Duane Miles <29195796+TheDeadlyPianist@users.noreply.github.com>
2019-12-16 12:56:55 +0000

    [DM] [NINOA-64] Updated as requested

--- project/CodeCoverageSettings.scala
+++ project/CodeCoverageSettings.scala
@@ -22,13 +22,9 @@ object CodeCoverageSettings {
   private val excludedPackages: Seq[String] = Seq(
     "<empty>",
     "Reverse.*",
-    ".*standardError*.*",
-    ".*govuk_wrapper*.*",
-    ".*main_template*.*",
     "uk.gov.hmrc.BuildInfo",
     "app.*",
     "prod.*",
-    "uk.gov.hmrc.vatsubscription.config.*",
     "testOnly.*",
     "api.Routes*",
     "api.RoutesPrefix*",
