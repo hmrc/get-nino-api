fde8b529f76008ecea66bda9b98af6d86e715a72
mithra-balakrishnan <177737570+mithra-balakrishnan@users.noreply.github.com>
2024-11-03 19:36:13 +0000

    DDCE-6162 - removing the commented out lines/updating latest dependency versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,14 +7,14 @@ object AppDependencies {
   private lazy val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                  %% "bootstrap-backend-play-30" % bootstrapPlayVersion,
     "uk.gov.hmrc"                  %% "play-hmrc-api-play-30"     % "8.0.0",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.17.2"
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.18.1"
   )
 
   private lazy val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"         %% "bootstrap-test-play-30" % bootstrapPlayVersion,
     "org.scalatest"       %% "scalatest"              % "3.2.19",
     "org.scalamock"       %% "scalamock"              % "6.0.0",
-    "org.wiremock"         % "wiremock-standalone"    % "3.9.1",
+    "org.wiremock"         % "wiremock-standalone"    % "3.9.2",
     "com.vladsch.flexmark" % "flexmark-all"           % "0.64.8"
   ).map(_ % Test)
 
