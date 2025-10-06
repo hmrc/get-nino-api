af84202090f359c01af60a439bfcdec124f8f578
Mark Beresford <mark.beresford@digital.hmrc.gov.uk>
2025-10-06 10:11:33 +0100

    DDCE-7046: Remove scalastyle plugin and update several deps

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import sbt.*
 
 object AppDependencies {
 
-  private lazy val bootstrapPlayVersion = "9.13.0"
+  private lazy val bootstrapPlayVersion = "10.2.0"
 
   private lazy val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                  %% "bootstrap-backend-play-30" % bootstrapPlayVersion,
     "uk.gov.hmrc"                  %% "play-hmrc-api-play-30"     % "8.0.0",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.19.1"
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"      % "2.20.0"
   )
 
   private lazy val test: Seq[ModuleID] = Seq(
