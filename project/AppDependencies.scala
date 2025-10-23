8b7c43caa5c27039dc795387af35c02844a9860e
mahesh <175945803+maheshhvr@users.noreply.github.com>
2025-10-23 12:32:00 +0100

    DDCE-7186 get-nino-api -Standardize repository.yaml Configuration Acr… (#144)
    * DDCE-7186 get-nino-api -Standardize repository.yaml Configuration Across all NW Services
    
    * DDCE-7186 get-nino-api -Standardize repository.yaml Configuration Across all NW Services Deps update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private lazy val bootstrapPlayVersion = "10.2.0"
+  private lazy val bootstrapPlayVersion = "10.3.0"
 
   private lazy val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                  %% "bootstrap-backend-play-30" % bootstrapPlayVersion,
